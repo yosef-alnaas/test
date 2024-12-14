e2e537ccf89b60d5c2cf8ec51e599cdd3449af4d
yosef-alnaas <115832068+yosef-alnaas@users.noreply.github.com>
2024-12-14 20:49:40 +0200

    test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const UserRoute = require("./router/user.router");
 const app = express();
 
+//wlierhf
 app.use(bodyParser.json())
 
 app.use("/",UserRoute);
