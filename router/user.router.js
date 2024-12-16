0e0504c48040accbaf990ce7b38f343aad182554
yosef-alnaas <115832068+yosef-alnaas@users.noreply.github.com>
2024-12-16 20:34:47 +0200

    fcyg

--- router/user.router.js
+++ router/user.router.js
@@ -4,6 +4,6 @@ const UserController = require("../controller/user.controller");
 router.post('/registration',UserController.register);
 router.post('/login',UserController.loginUserController);
 router.get('/findUsers',UserController.findusers);
-
+   
 
 module.exports=router;
